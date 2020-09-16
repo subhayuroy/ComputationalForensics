1c4d80d30f94c3493ba608a7894bbf36e8048e0a
subhayuroy <subhayu.roy2001@gmail.com>
2020-09-16 21:57:58 +0530

    Cracking an Encryption completed

---
+++ Cracking an Encryption/cracking.py
@@ -0,0 +1,22 @@
+import sys
+
+def decrypt(k, cipher):
+    plaintext = ''
+    for each in cipher:
+        p = (ord(each) - k) % 126
+
+        if p < 32:
+            p += 95
+            plaintext += chr(p)
+            print(plaintext)
+
+def main(argv):
+    if len(sys.argv) != 1:
+        sys.exit('Usage: cracking.py')
+        cipher = input("Enter message= ")
+
+        for i in range(1, 95):
+            decrypt(i, cipher)
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
